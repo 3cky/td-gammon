73627c209ab87a1fe5ba8d87944ccafb7a8c902f
Victor Antonovich <v.antonovich@gmail.com>
2018-03-19 17:34:03 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+PROJECT = 'td-gammon'
+setup(
+    name=PROJECT,
+    packages=[
+        'gammon',
+        'models',
+        '',
+    ],
+    version='0.1.0',
+
+    url='https://github.com/3cky/td-gammon',
+)
